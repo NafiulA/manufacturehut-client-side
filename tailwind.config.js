66f694c2f58eeda03a52ac7e5d25fd42bba54de5
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-22 15:45:07 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["corporate"]
+  },
+  plugins: [require("daisyui")],
+}
