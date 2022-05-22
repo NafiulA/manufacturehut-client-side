19ce791dcd33995b795ac293881b9e7e835d360c
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-22 16:57:25 +0600

    header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Authentication/Login';
+import Signup from './Pages/Authentication/Signup';
+import Blogs from './Pages/Blogs/Blogs';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Home from './Pages/Home/Home';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
-    <div>
-
+    <div className='app'>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/portfolio' element={<Portfolio></Portfolio>}></Route>
+      </Routes>
     </div>
   );
 }
