a580dd88505d1a11d938bb77ba07a7b7bca0505a
Nafiul Alam <nafiulalam30@gmail.com>
2022-06-12 18:41:06 +0600

    loading on fetch updated

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import Loading from '../Shared/Loading';
 import ReviewCard from './ReviewCard';
 
 const Reviews = () => {
+    const [reviewLoading, setReviewLoading] = useState(true);
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
@@ -9,12 +11,14 @@ const Reviews = () => {
             .then(res => res.json())
             .then(data => {
                 setReviews(data.reverse());
+                setReviewLoading(false);
             })
     }, []);
 
     return (
         <div className='my-6 py-5'>
             <p className='text-center text-3xl my-5'>Reviews</p>
+            {reviewLoading && <Loading></Loading>}
             <div className='w-full md:w-3/4 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center'>
                 {reviews.slice(0, 6).map(review => <ReviewCard
                     key={review._id}
